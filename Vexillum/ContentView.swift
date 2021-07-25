4a85f75a4ee715820d5e799814268090191d72df
Saad <saadmohammedanis@gmail.com>
2021-07-25 09:42:45 +0300

    Initial Commit

---
+++ Vexillum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vexillum
+//
+//  Created by Saad Anis on 7/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vexillum/VexillumApp.swift
@@ -0,0 +1,17 @@
+//
+//  VexillumApp.swift
+//  Vexillum
+//
+//  Created by Saad Anis on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct VexillumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
